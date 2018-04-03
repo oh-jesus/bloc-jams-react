2a8387d18f17c609a202b1617cfe3a68f986ed9c
Jesus A Martinez <martinez18.rmhs@gmail.com>
2018-04-02 19:07:51 -0700

    routing and album

---
+++ src/components/Album.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Album = () => (
+  <section className="Album">
+    these are the breaks
+  </section>
+);
+
+export default Album;
---
+++ src/components/Landing.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Landing = () => (
+  <section className="Landing">
+  landing page will go here
+  </section>
+);
+
+export default Landing;
