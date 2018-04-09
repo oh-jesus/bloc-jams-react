cf062a14c1c90adf010cd6893b8ca85564bb827b
Jesus A Martinez <martinez18.rmhs@gmail.com>
2018-04-08 17:34:35 -0700

    revision

--- src/components/Album.js
+++ src/components/Album.js
@@ -65,8 +65,15 @@ class Album extends Component {
          <tbody>
             {this.state.album.songs.map( (song, index) =>
              <tr className="song" key={index} onclick={() => this.handleSongClick(song)} >
+               <td className="song-actions">
+                   <button>
+                     <span className="song-number">{index+1}</span>
+                     <span className="ion-play"></span>
+                     <span className="ion-pause"></span>
+                   </button>
+               </td>
                <td className="song-title">{song.title}</td>
-                 <td className="song-duration">{song.duration}</td>
+               <td className="song-duration">{song.duration}</td>
              </tr>
              )}
          </tbody>
